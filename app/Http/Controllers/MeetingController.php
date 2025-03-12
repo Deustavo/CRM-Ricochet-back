17647bdc9f5da45a12063e88b490989f3835ff0f
gustavo.andrade <gustavo.andrade@tray.net.br>
2025-03-11 21:16:08 -0300

    Rotas base

---
+++ app/Http/Controllers/MeetingController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class MeetingController extends Controller
+{
+    public function index()
+    {
+       dd('MeetingController@index');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MeetingController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('meetings', MeetingController::class);
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [UserController::class, 'user']);
+});
